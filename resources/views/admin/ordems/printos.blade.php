bbee7e3166b9fa2333656e1364fc3dabf26e9efb
Wilson Missina Faria <wmfaria@outlook.com>
2022-01-03 15:48:44 -0300

    Melhoria de interface de impressao

--- resources/views/admin/ordems/printos.blade.php
+++ resources/views/admin/ordems/printos.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.admin')
 
 @section('content')
+<style>
+@media print {
+  div {
+    break-inside: avoid;
+  }
+}
+</style>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@500&display=swap" rel="stylesheet">
